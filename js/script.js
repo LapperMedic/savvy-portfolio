fed37691d22ba89c291e8bffccae4dfb59f73887
LapperMedic <alexrlapp@gmail.com>
2018-08-27 21:15:50 -0500

    More JS

--- js/script.js
+++ js/script.js
@@ -9,16 +9,31 @@ var greeting = function greeting(){
     }
 };
 
+var bill = {
+    'type': 'restaurant',
+    'items': 5,
+    'total': 67.94,
+    'tax': 1.12,
+    'tip': 1.2,
+    'paid': true
+};
+
 var fibonacci = function fibonacci(last){
     var former = 1;
     var latter = 1;
-    var i;
+    var i = 0;
+    var final = [];
 
-    for(i = 0; i <= last; i++){
-        console.log(former, latter);
+    for(i = 0; i < last; i += 2){
+        final[i] = former;
+        final[i + 1] = latter;
         former += latter;
         latter += former;
     }
+
+    console.log(final);
 };
 
 greeting();
+fibonacci(5);
+console.log('String', 1, true, undefined, null);

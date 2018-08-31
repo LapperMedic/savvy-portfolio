2b1e02a491a779f3a6a9cc78764fa2255bd43d79
LapperMedic <alexrlapp@gmail.com>
2018-08-30 21:29:16 -0500

    Added h1 JS easter egg

--- js/index.js
+++ js/index.js
@@ -5,12 +5,18 @@ import Footer from '../components/Footer';
 import Greeting from './script.js';
 
 var state = {
-    'nav': '',
-    'header': 'Alex Lapp',
-    'body': '',
-    'footer': ''
+    'header': 'Alex Lapp'
 };
 
 document.querySelector('#root').innerHTML = `${Nav()} ${Header(state)} ${Body()} ${Footer()}`;
 
 Greeting();
+
+document.querySelectorAll('h1')[0].addEventListener('click', (event) => {
+    if(event.target.textContent == state.header){
+        event.target.textContent = 'OH BOI';
+    }
+    else{
+        event.target.textContent = state.header;
+    }
+});

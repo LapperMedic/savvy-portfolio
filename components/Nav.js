d08029e07eef2beacf946f989001d5cc8dda9dc7
LapperMedic <alexrlapp@gmail.com>
2018-09-11 20:00:31 -0500

    Added navigo funcationality

--- components/Nav.js
+++ components/Nav.js
@@ -2,7 +2,13 @@ export default function Nav(state){
     var navLinks = '<div id="nav"><ul>'; // Begin the navbar
 
     for(let i = 0; i < state.links.length; i++){ // For each index in the navbar links array
-        navLinks += `<li><a href="/${state.links[i]}">${state.links[i]}</a></li>`; // Make a li that links to it
+        if(state.links[i] !== 'home'){ // Make a li that links to it
+            navLinks += `<li><a href="/${state.links[i]}" data-navigo>${state.links[i]}</a></li>`;
+        }
+        
+        else{
+            navLinks += `<li><a href="/" data-navigo>${state.links[i]}</a></li>`;
+        }
     }
 
     navLinks += '</ul></div>'; // End the navbar
--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+import Navigo from 'navigo';
 import Nav from '../components/Nav';
 import Header from '../components/Header';
 import Body from '../components/Body';
@@ -5,23 +6,22 @@ import Footer from '../components/Footer';
 import * as State from '../store';
 
 var root = document.querySelector('#root');
+var router = new Navigo(window.location.origin);
 
 function render(state){
-    var links;
-
     root.innerHTML = `${Nav(state[state.active])} ${Header(state[state.active])} ${Body(state[state.active])} ${Footer()}`;
-    links = document.querySelectorAll('#nav a'); // An array containing the navbar links
-    for(let i = 0; i < links.length; i++){
-        links[i].addEventListener('click', handleNav); // Add an event listener to each navbar link
-    }
+
+    router.updatePageLinks();
 }
 
-function handleNav(event){
-    var newState = Object.assign({}, State); // Catch the state we currently have
+function handleNav(activePage){
+    var newState = Object.assign({}, State);
 
-    event.preventDefault(); // Don't navigate to a new page
-    newState.active = event.target.textContent.toLowerCase(); // Change the state to the text within the navbar link
-    render(newState); // Re-render the page
+    newState.active = activePage;
+    render(newState);
 }
 
-render(State);
+router
+    .on('/:page', (params) => handleNav(params.page))
+    .on('/', () => handleNav('home'))
+    .resolve();
